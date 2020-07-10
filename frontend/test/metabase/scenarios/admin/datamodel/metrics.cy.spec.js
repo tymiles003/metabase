23967e778c68bc39e62dd818347518acd6c4506a
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-07-10 16:45:55 -0700

    fixed metrics.cy to new frontend

--- frontend/test/metabase/scenarios/admin/datamodel/metrics.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/datamodel/metrics.cy.spec.js
@@ -8,21 +8,19 @@ describe("scenarios > admin > datamodel > metrics", () => {
   });
 
   it("should create a metric", () => {
-    cy.visit("/admin");
-    cy.contains("Data Model").click();
-    cy.contains("Metrics").click();
-    cy.contains("New metric").click();
-    cy.contains("Select a table").click();
-    popover()
-      .contains("Orders")
-      .click({ force: true }); // this shouldn't be needed, but there were issues with reordering as loads happeend
+    cy.visit("/admin/datamodel/database/1");
+    cy.findByText("Orders").click();
+    cy.wait(2000)
+      .contains("Add a Metric")
+      .click();
 
-    cy.url().should("match", /metric\/create$/);
+    cy.url().should("match", /metric\/create/);
     cy.contains("Create Your Metric");
 
     // filter to orders with total under 100
-    cy.contains("Add filters").click();
-    cy.contains("Total").click();
+    cy.get(".Icon-add")
+      .click();
+    cy.findByText("Total").click();
     cy.contains("Equal to").click();
     cy.contains("Less than").click();
     cy.get('[placeholder="Enter a number"]').type("100");
@@ -30,7 +28,6 @@ describe("scenarios > admin > datamodel > metrics", () => {
       .contains("Add filter")
       .click();
 
-    //
     cy.contains("Result: 12765");
 
     // fill in name/description
@@ -41,21 +38,17 @@ describe("scenarios > admin > datamodel > metrics", () => {
 
     // saving bounces you back and you see new metric in the list
     cy.contains("Save changes").click();
-    cy.url().should("match", /datamodel\/metrics$/);
     cy.contains("orders <100");
     cy.contains("Count, Filtered by Total");
   });
 
   it("should update that metric", () => {
-    cy.visit("/admin");
-    cy.contains("Data Model").click();
-    cy.contains("Metrics").click();
-
-    cy.contains("orders <100")
-      .parent()
-      .parent()
+    cy.visit("/admin/datamodel/database/1");
+    cy.findByText("Orders").click();
+    
+    cy.get("#MetricsList")
       .find(".Icon-ellipsis")
-      .click();
+      .click()
     cy.contains("Edit Metric").click();
 
     // update the filter from "< 100" to "> 10"
@@ -89,13 +82,11 @@ describe("scenarios > admin > datamodel > metrics", () => {
     cy.contains("Save changes").click();
 
     // get redirected to previous page and see the new metric name
-    cy.url().should("match", /datamodel\/metrics$/);
+    cy.url().should("match", /datamodel\/database/);
     cy.contains("orders >10");
 
     // clean up
-    cy.contains("orders >10")
-      .parent()
-      .parent()
+    cy.get("#MetricsList")
       .find(".Icon-ellipsis")
       .click();
     cy.contains("Retire Metric").click();
